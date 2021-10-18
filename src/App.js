e3908aa861b48b8184a78ed037f04a991fc28651
puzanovkirill <austinscottbeatz@gmail.com>
2021-10-18 22:13:45 +0500

    feat: form events handeled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
     return (
         <div className="App">
             <header>My todo list</header>
-            <Form/>
+            <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
         </div>
     );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({todos, setTodos, inputText, setInputText}) => {
+    const inputTextHandeler = (e) => {
+        setInputText(e.target.value);
+    }
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
+        ]);
+        setInputText('');
+    }
     return (
         <form className="todo-form">
-            <input type="text" className="todo-input" placeholder="Add new..."/>
-            <button className="todo-button" type="submit">Add</button>
+            <div className="input-box">
+                <input value={inputText} onChange={inputTextHandeler} type="text" className="todo-input" placeholder="Add new..."/>
+                <button onClick={submitTodoHandler} className="todo-button" type="submit">Add</button>
+            </div>
             <hr/>
             <div className="select">
                 <div className="filter-header">Filter</div>
