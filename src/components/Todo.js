faf5229c6bf52ab14ef86214a177ec6e66cb5fd8
puzanovkirill <austinscottbeatz@gmail.com>
2021-10-19 19:47:20 +0300

    feat: using checkbox instead of button to complete todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,18 +4,20 @@ const Todo = ({todos, todo, setTodos}) => {
     const deleteHandler = () => {
         setTodos(todos.filter(item => item.id !== todo.id));
     }
-    const competeHandler = () => {
+    const completeHandler = () => {
         setTodos(todos.map(item => {
-            if(item.id === todo.id)
+            if (item.id === todo.id)
                 return {...item, completed: !item.completed};
             return item;
         }));
     }
     return (
         <div className="todo">
-            <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>{todo.text}</li>
-            <button onClick={competeHandler} className="todo-complete-btn">Complete</button>
-            <button onClick={deleteHandler} className="todo-delete-btn">Delete</button>
+            <div className="todo-check">
+                <input type="checkbox" className="todo-checkbox" onClick={completeHandler}/>
+                <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>{todo.text}</li>
+            </div>
+                <button onClick={deleteHandler} className="todo-delete-btn">Delete</button>
         </div>
     );
 };
